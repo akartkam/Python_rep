4f07298914fbd1ace7542d256ab238433f5bbc2b
Artur Akchurin <akartkam@gmail.com>
2013-11-26 16:42:08 +0400

    def makemap

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
-print("My threed git commit")
+﻿def makemap(fstr, tostr):
+    transmap = {}
+    for i, (fchar, tochar) in enumerate(zip(fstr, tostr)):
+        if tochar in " ":
+    	    tochar=""
+        if fchar in " ":
+            while fstr[i] in " ":
+                i -= 1
+            transmap[fstr[i]] += tochar
+        else:
+    	    transmap[fchar] = tochar
+    return transmap
+
+if __name__ in "__main__":
+    print(makemap("йцукя чс", "qweryaxc"))
