b0ed9079c5d0aba07b8e0a78fe79016cca3d02be
Artur Akchurin <akartkam@gmail.com>
2013-11-26 22:12:25 +0400

    Second edition with classes

--- test2.py
+++ test2.py
@@ -1,1 +1,27 @@
-class A: pass
+class Parent():
+    def setdata(self, data):
+        self.data = data
+
+    def show(self):
+        print(self.data)
+
+    def __add__(self, other):
+        return self.data + " " + other.data
+
+class Childe(Parent):
+    def show(self):
+        print(self.data + "| from Childe")
+    
+
+myexp1 = Parent()
+myexp2 = Childe()
+
+myexp1.setdata("My first obj")
+myexp2.setdata("My second obj")
+
+myexp1.show()
+myexp2.show()
+
+print(myexp1 + myexp2)
+
+
