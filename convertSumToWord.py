2d38d7183603d74994d18413a534aa940619663a
Artur Akchurin <akartkam@gmail.com>
2013-11-29 17:28:13 +0400

    Sum toWord is working

--- convertSumToWord.py
+++ convertSumToWord.py
@@ -16,10 +16,75 @@ def TriplToWord(triplet):
 
     return s + edin[triplet]
 
-while True:
-    try:
-        x = int(input("Введите, пожалуйста, трехзначное положительное число: "))
-        print(TriplToWord(x))
-    except:
-        print("Ой!  Это некорректное число.  Попробуйте ещё раз...")
-        raise
+
+def SumToWord(summa):
+    if summa == 0:
+        return 'Ноль руб. 00 коп.'
+    intSum = int(summa)
+    wordSum = ''
+    if intSum > 999999999:
+        divSum =  intSum // 1000000000
+        wordSum = TriplToWord(divSum)
+        if ((divSum-(divSum // 100)*100)>10) and ((divSum-(divSum // 100)*100)<20):
+            wordSum += ' миллиардов'
+        elif (divSum % 10) == 1:
+            wordSum += ' миллиард'
+        elif ((divSum % 10)>=2)and((divSum % 10)<=4):
+            wordSum += ' миллиарда'
+        else:
+            wordSum += ' миллиардов'
+        intSum = intSum % 1000000000
+
+    if intSum > 999999:
+        divSum =  intSum // 1000000
+        wordSum += ' ' + TriplToWord(divSum)
+        if ((divSum-(divSum // 100)*100)>10) and ((divSum-(divSum // 100)*100)<20):
+            wordSum += ' миллионов'
+        elif (divSum % 10) == 1:
+            wordSum += ' миллион'
+        elif ((divSum % 10)>=2)and((divSum % 10)<=4):
+            wordSum += ' миллиона'
+        else:
+            wordSum += ' миллионов'
+        intSum = intSum % 1000000
+
+    if intSum > 999:
+        divSum =  intSum // 1000
+        wordSum += ' ' + TriplToWord(divSum)
+        if ((divSum-(divSum // 100)*100)>10) and ((divSum-(divSum // 100)*100)<20):
+            wordSum += ' тысяч'
+        elif (divSum % 10) == 1:
+            wordSum = wordSum[:-2]
+            wordSum += 'на тысяча'
+        elif (divSum % 10) == 1:
+              wordSum = wordSum[:-1]
+              wordSum += 'е тысячи'
+        elif ((divSum % 10)>=3)and((divSum % 10)<=4):
+              wordSum += ' тысячи'
+        else:
+            wordSum += ' тысяч'
+        intSum = intSum % 1000
+    
+    divSum = intSum
+    wordSum += ' ' + TriplToWord(divSum) + ' руб.'
+
+    divSum = int((summa - int(summa))*100)
+    wordSum += ' ' + str(divSum) + ' коп.'
+    wordSum = wordSum.strip()
+    wordSum = wordSum[0:1].upper() + wordSum[1:]
+    return wordSum
+            
+
+    
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            x = float(input("Введите, пожалуйста, сумму , меньше 1 триллиона число: "))
+            if x < 0:
+                raise
+            print(SumToWord(x))
+        except:
+            print("Ой!  Это некорректное число.  Попробуйте ещё раз...")
+            
